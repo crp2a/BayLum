2f7730b041d8b0059443995b6a47f6dd7385be0b
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-01-14 19:42:48 +0100

    Minor code polshing.

--- R/Age_OSLC14.R
+++ R/Age_OSLC14.R
@@ -239,20 +239,21 @@
 #' # OSL data
 #' data(DATA1,envir = environment())
 #' data(DATA2,envir = environment())
-#' Data=Concat_DataFile(DATA2,DATA1)
+#' Data <- Concat_DataFile(DATA2,DATA1)
 #'
 #' # 14C data
 #  data(DATA_C14,envir = environment())
-#' C14Cal=DATA_C14$C14[1,1]
-#' SigmaC14Cal=DATA_C14$C14[1,2]
-#' Names=DATA_C14$Names[1]
+#' C14Cal <- DATA_C14$C14[1,1]
+#' SigmaC14Cal <- DATA_C14$C14[1,2]
+#' Names <- DATA_C14$Names[1]
 #'
 #' # Prior Age
 #' prior=rep(c(1,60),3)
 #' samplenature=matrix(data=c(1,0,1,0,1,0),ncol=3,nrow=2,byrow=TRUE)
-#' SC=matrix(data=c(1,1,1,0,1,1,0,0,1,0,0,0),ncol=3,nrow=4,byrow=TRUE)
+#' SC <- matrix(data=c(1,1,1,0,1,1,0,0,1,0,0,0),ncol=3,nrow=4,byrow=TRUE)
+#'
 #' ## Age computation of samples
-#' Age=Age_OSLC14(DATA=Data,Data_C14Cal=C14Cal,Data_SigmaC14Cal=SigmaC14Cal,
+#' Age <- Age_OSLC14(DATA=Data,Data_C14Cal=C14Cal,Data_SigmaC14Cal=SigmaC14Cal,
 #'    SampleNames=c("GDB5",Names,"GDB3"),Nb_sample=3,SampleNature=samplenature,
 #'    PriorAge=prior,StratiConstraints=SC,Iter=50,Nb_chaines=2)
 #' @export
