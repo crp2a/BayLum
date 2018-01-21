805da70411c0396a7a4a71c40dd4fa391fcf9765
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-01-21 12:01:28 +0100

    Code and language polish SC_Ordered()

--- R/SC_ordered.R
+++ R/SC_ordered.R
@@ -1,30 +1,34 @@
-#' Stratigraphic matrix for samples ordered
+#' Create stratigraphically ordered sample matrix
 #'
-#' Construc the stratigraphic matrix used in fonction \code{AgeS_Computation} and \code{AgeC14_Computation}
-#' for samples that are all ordered by increasing oder.
+#' Construct the stratigraphic matrix used in the functions [AgeS_Computation] and [AgeC14_Computation]
+#' for samples that are all ordered by increasing age.
 #'
-#' @param Nb_sample  interger: the sample number.
+#' @param Nb_sample [integer] (**required**): the number of samples
 #'
-#' @return Stratigraphic matrix where each sample are ordered by incresing order
-#' This matrix can be intergrate in \code{AgeS_Computation} function.
-#' We refer to detail on \code{AgeS_Computation} for more information concerning this matrix.
+#' @return Stratigraphic matrix where each sample are ordered by increasing order.
+#' This matrix can be intergrated in the function [AgeS_Computation].
+#' Please see [AgeS_Computation] for more information on this matrix.
 #'
-#' @seealso \code{AgeS_Computation}, \code{SC_Matrix}
+#' @seealso [AgeS_Computation], [SCMatrix]
 #'
-#' @author Claire Christophe, Anne Philippe, Guillaume Guerin
+#' @author Claire Christophe, Anne Philippe, Sebastian Kreutzer, Guillaume Guérin
 #'
 #' @examples
 #' # compute the stratigraphic matrix for 3 samples such that the first sample is younger
 #' # than the second, and the second is younger than the third
-#' (SC=SC_Ordered(Nb_sample=3))
+#' SC <- SC_Ordered(Nb_sample = 3)
 #'
+#' @keywords datagen
+#'
+#' @md
 #' @export
-
-SC_Ordered<-function(Nb_sample){
-  SC=matrix(data=0,ncol=Nb_sample,nrow=(Nb_sample+1))
-  SC[1,]=rep(1,Nb_sample)
-  for(i in 1:Nb_sample){
-    SC[i+1,]=c(rep(0,i),rep(1,(Nb_sample-i)))
+SC_Ordered <- function(Nb_sample) {
+  SC <- matrix(data = 0,
+               ncol = Nb_sample,
+               nrow = (Nb_sample + 1))
+  SC[1, ] <- rep(1, Nb_sample)
+  for (i in 1:Nb_sample) {
+    SC[i + 1, ] <- c(rep(0, i), rep(1, (Nb_sample - i)))
   }
   return(SC)
 }
