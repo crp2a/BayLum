465e4f3781f573d64dc4075c4862a535364653f2
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2018-01-26 18:34:58 +0100

    Package rebuild and webpage

--- R/BayLum-package.R
+++ R/BayLum-package.R
@@ -1,26 +1,27 @@
-#' @title Chronological Bayesian Models Integrating Optically Stimulated Luminescence and 14C Age Dating
+#' @title Chronological Bayesian Models Integrating Optically Stimulated Luminescence and 14C Dating
 #'
-#' @description A collection of various R functions for Bayesian analysis of luminescence data and 14C Cal age estimate.
-#' This includes, amongst others, data import, export, application of age models and palaeodose model.
+#' @description A collection of various R functions for Bayesian analysis of luminescence data and 14C age estimates.
+#' This includes, amongst others, data import, export, application of age and palaeodose models.
 #'
 #' @name BayLum-package
 #' @aliases BayLum-package BayLum
 #' @docType package
-#' @author Claire Christophe, Anne Philippe, Sebastian Kreutzer, Guillaume Guerin
+#' @author Claire Christophe, Anne Philippe, Sebastian Kreutzer, Guillaume Guérin
 #' @keywords package
-#' @details This package is based on the functions: \code{\link{Generate_DataFile}} and \code{\link{Generate_DataFile_MG}}
+#' @details This package is based on the functions: [Generate_DataFile] and [Generate_DataFile_MG]
 #' to import luminisecence data. These functions create a list containing all informations to compute age
 #' of Single-grain OSL measurements for the first function and Multi-grain OSL measurements for the second.
 #'
-#' The functions:\code{\link{Age_Computation}} and \code{\link{AgeS_Computation}} use Bayesian analysis for OSL age
+#' The functions: [Age_Computation] and [AgeS_Computation] use Bayesian analysis for OSL age
 #' estimation for one or various samples according to differents models
-#' (e.g. different growth curves and different equivalent dose distributions around the palaeodose).
+#' (e.g. different dose-response curves and different equivalent dose distributions around the palaeodose).
 #'
-#' It is possible to consider various Bin files per sample, to compute ages of samples in stratigraphic constraints
+#' It is possible to consider various BIN-files per sample, to compute ages of samples in stratigraphic constraints
 #' and to integrate systematic errors.
 #'
-#' It is possible to calibrate 14C age with the function \code{\link{AgeC14_Computation}}.
-#' We can also estimate chronology containing 14C age and OSL samples with the function \code{\link{Age_OSLC14}}.
+#' It is possible to calibrate 14C age with the function [AgeC14_Computation].
+#' We can also estimate chronology containing 14C age and OSL samples with the function [Age_OSLC14].
 #'
+#' @md
 #' @import utils stats methods graphics grDevices coda
 NULL
