ba86e9a28a785cfb8f4dc35d862ce310e7c9685d
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-01-02 20:02:22 +0100

    The date was not updated, this does not make sense

--- BayLum.BuildScripts/BayLum.PBS_knitr.R
+++ BayLum.BuildScripts/BayLum.PBS_knitr.R
@@ -8,20 +8,34 @@
 if(!require("knitr"))
   install.packages("knitr")
 
-##set new version number (count one up)
+# Set new date and version number -----------------------------------------------------------------
+##read DESCRIPTION
 temp <- readLines("DESCRIPTION")
+
+##grep date and version number
+version_date <- grep(pattern = "Date:", x = temp, fixed = TRUE)
 version_id <- grep(pattern = "Version:", x = temp, fixed = TRUE)
+
+##update date and version number
+temp[version_date] <- paste0("Date: ", Sys.Date())
+
 version_number <-
-  unlist(strsplit(x = temp[version_id], split = ".", fixed =
-                    TRUE))
-version_number <- paste(c(version_number[-length(version_number)],
-                          as.numeric(version_number[length(version_number)]) + 1), collapse = ".")
+  unlist(strsplit(x = temp[version_id], split = ".", fixed = TRUE))
+version_number <- paste(
+  c(version_number[-length(version_number)],
+    as.numeric(version_number[length(version_number)]) + 1),
+  collapse = ".")
+
 temp[version_id] <- version_number
+
+##write back to file
 writeLines(text = temp, con = "DESCRIPTION")
 
-#knit NEWS
+
+# Knit NEWS -----------------------------------------------------------------------------------
 rmarkdown::render("NEWS.Rmd", output_format = "github_document")
 
-##CLEAN-UP
+
+# Houskeeping ---------------------------------------------------------------------------------
 if(file.exists("NEWS.html"))
   file.remove("NEWS.html")
