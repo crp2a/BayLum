e2b89e4aa6a372d3a5ebd69fcd290f2dfa98f5cf
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2024-09-18 01:13:03 +0200

    Rebuild vignette

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
 # Vignettes that relies on heavy computations have been precompiled
 old_wd <- setwd("./vignettes")
-knitr::knit("BayLum.Rmd.orig", output = "BayLum.Rmd")
+knitr::knit("BayLum.Rmd.orig", output = "BayLum.Rmd", envir = new.env())
 setwd(old_wd)
